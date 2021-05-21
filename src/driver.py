b9e0097a16c02949e671137738c8b6b0d1b6d9e7
begranger <ben.granger9@gmail.com>
2021-05-21 00:21:27 -0700

    Finished out full-flow driver, updated extract...sh w absolute paths

--- src/driver.py
+++ src/driver.py
@@ -3,12 +3,31 @@ import filetools as ft;
 import musictools as mt;
 
 # Unpack the archive
-##sp.run('./extract_drive_dumps.sh')
+run_unpack = input('Unpack archive? (y/n): ');
+if run_unpack == 'y':
+	print('Unpacking archive ... ', end='');
+	sp.run('./extract_drive_dumps.sh');
+	print('Done');
 
+# Continue to excel generation?
+run_excel_gen = input('Generate songs.xlsx? (y/n): ');
+if run_excel_gen == 'n':
+	quit();
+
+# Generate FileList of all songs
 srcRoot = '/home/ben/Music/CleanedBackup';
 fl = ft.FileList(srcRoot);
+print('Num songs = ' + str(len(fl.files)));
 
-mt.dumpToExcel(fl, '/home/ben/Music/song.xlsx');
+# Check for empty FileList
+if len(fl.files) == 0:
+	print('Error: no songs found, exiting');
+	quit();
+
+# Extract metadata and dump to excel
+print('Extracting metadata and dumping to excel ... ', end='');
+mt.dumpToExcel(fl, '/home/ben/Music/songs.xlsx');
+print('Done');
 
 
 
--- src/extract_drive_dumps.sh
+++ src/extract_drive_dumps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 unzip ~/Downloads/cleaned_zips/Cleaned\ Backup\ \(old\ and\ duplicates\ deleted\)-20200102T054547Z-00\*.zip -d ~/Music/
-mv ./Cleaned\ Backup\ \(old\ and\ duplicates\ deleted\) ./CleanedBackup
+mv ~/Music/Cleaned\ Backup\ \(old\ and\ duplicates\ deleted\) ~/Music/CleanedBackup
